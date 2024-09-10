8f57179b48c3320036e9892dbfaf8a9891a0f135
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2024-09-10 12:15:23 +0300

    add loading example

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -6,6 +6,16 @@ function Main()
 {
     return (
         <div className="row">
+            {/* <div className="row" style={{textAlign: "center"}} id = "loading">
+                <p>
+                <div className="col-md-12">
+                    <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+                </p>
+            </div> */}
+
             <div className="col-md-12">
                 <p><h3>Start page</h3></p>
             </div>
