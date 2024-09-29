34a4cae681702bf43e4d2249db4003685c3e6baa
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2024-09-29 21:05:16 +0300

    add formatter settings

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,30 +1,24 @@
-import React, { useEffect } from 'react';
-import '../scss/styles.scss';
+import React, { useEffect } from 'react'
+import '../scss/styles.scss'
 
+function Main() {
+  let refreshIntervalId: NodeJS.Timeout
 
+  useEffect(() => {
+    refreshIntervalId = setInterval(() => {
+      console.log('Main...')
+    }, 1000)
 
+    return () => {
+      // код выполняется при размонтировании компонента (закрытии)
+      console.log('Component unmounted')
+      clearInterval(refreshIntervalId)
+    }
+  })
 
-function Main()
-{
-    let refreshIntervalId:NodeJS.Timeout;
-
-    useEffect(() => {
-        
-        refreshIntervalId = setInterval(()=>{
-            console.log("Main...")
-        }, 1000);
-
-        return () => {
-            // код выполняется при размонтировании компонента (закрытии)
-            console.log('Component unmounted');
-            clearInterval(refreshIntervalId);
-          };
-    });
-
-
-    return (
-        <div className="row">
-            {/* <div className="row" style={{textAlign: "center"}} id = "loading">
+  return (
+    <div className="row">
+      {/* <div className="row" style={{textAlign: "center"}} id = "loading">
                 <p>
                 <div className="col-md-12">
                     <div className="spinner-border" role="status">
@@ -34,12 +28,16 @@ function Main()
                 </p>
             </div> */}
 
-            <div className="col-md-12">
-                <p><h3>Start page</h3></p>
-            </div>
-            <a href="#" className="btn btn-primary" role = "button">Click me</a>
-        </div>
-    );
+      <div className="col-md-12">
+        <p>
+          <h3>Start page</h3>
+        </p>
+      </div>
+      <a href="#" className="btn btn-primary" role="button">
+        Click me
+      </a>
+    </div>
+  )
 }
 
-export default Main;
+export default Main
